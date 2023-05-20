5d2ec5eb1940ed7d5efba13d58735457f03ed4f8
Benjamin <bnkaiser04@gmail.com>
2023-05-20 14:21:49 -0400

    added images to day 2 and animations to all days

--- script.js
+++ script.js
@@ -1,28 +1,110 @@
 function updatemenu() {
-    if (document.getElementById('responsive-menu').checked == true) {
-      document.getElementById('menu').style.borderBottomRightRadius = '0';
-      document.getElementById('menu').style.borderBottomLeftRadius = '0';
-    }else{
-      document.getElementById('menu').style.borderRadius = '0px';
-    }
+  if (document.getElementById('responsive-menu').checked == true) {
+    document.getElementById('menu').style.borderBottomRightRadius = '0';
+    document.getElementById('menu').style.borderBottomLeftRadius = '0';
+  } else {
+    document.getElementById('menu').style.borderRadius = '0px';
   }
+}
 
-  window.addEventListener('resize', function() {
-    if (window.innerWidth <= 762) {
-      document.getElementById('warning-message').style.display = 'block';
-      document.body.style.overflow = 'hidden';
-    } else {
-      document.getElementById('warning-message').style.display = 'none';
-      document.body.style.overflow = 'auto';
-    }
-  });
-
-var tl = gsap.timeline({paused: true});
-
-tl.to(".day1", {"--scale": 1.2, duration: 1, transformOrigin: "center"});
-document.querySelector(".day1Link").addEventListener("mouseover", function() {
- tl.play();
+window.addEventListener('resize', function () {
+  if (window.innerWidth <= 762) {
+    document.getElementById('warning-message').style.display = 'block';
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.getElementById('warning-message').style.display = 'none';
+    document.body.style.overflow = 'auto';
+  }
 });
-document.querySelector(".day1Link").addEventListener("mouseout", function() {
- tl.reverse();
+
+var tl1 = gsap.timeline({ paused: true });
+var tl2 = gsap.timeline({ paused: true });
+var tl3 = gsap.timeline({ paused: true });
+var tl4 = gsap.timeline({ paused: true });
+var tl5 = gsap.timeline({ paused: true });
+var tl6 = gsap.timeline({ paused: true });
+var tl7 = gsap.timeline({ paused: true });
+var tl8 = gsap.timeline({ paused: true });
+var tl9 = gsap.timeline({ paused: true });
+var tl10 = gsap.timeline({ paused: true });
+
+
+tl1.to(".day1", { "--scale": 1.2, duration: 0.5, transformOrigin: "center", ease: "circ.inOut", });
+document.querySelector(".day1Link").addEventListener("mouseover", function () {
+  tl1.play();
+});
+document.querySelector(".day1Link").addEventListener("mouseout", function () {
+  tl1.reverse();
+});
+
+tl2.to(".day2", { "--scale": 1.2, duration: 0.5, transformOrigin: "center", ease: "circ.inOut", });
+document.querySelector(".day2Link").addEventListener("mouseover", function () {
+  tl2.play();
+});
+document.querySelector(".day2Link").addEventListener("mouseout", function () {
+  tl2.reverse();
+});
+
+tl3.to(".day3", { "--scale": 1.2, duration: 0.5, transformOrigin: "center", ease: "circ.inOut", });
+document.querySelector(".day3Link").addEventListener("mouseover", function () {
+  tl3.play();
+});
+document.querySelector(".day3Link").addEventListener("mouseout", function () {
+  tl3.reverse();
+});
+
+tl4.to(".day4", { "--scale": 1.2, duration: 0.5, transformOrigin: "center", ease: "circ.inOut", });
+document.querySelector(".day4Link").addEventListener("mouseover", function () {
+  tl4.play();
+});
+document.querySelector(".day4Link").addEventListener("mouseout", function () {
+  tl4.reverse();
+});
+
+tl5.to(".day5", { "--scale": 1.2, duration: 0.5, transformOrigin: "center", ease: "circ.inOut", });
+document.querySelector(".day5Link").addEventListener("mouseover", function () {
+  tl5.play();
+});
+document.querySelector(".day5Link").addEventListener("mouseout", function () {
+  tl5.reverse();
+});
+
+tl6.to(".day6", { "--scale": 1.2, duration: 0.5, transformOrigin: "center", ease: "circ.inOut", });
+document.querySelector(".day6Link").addEventListener("mouseover", function () {
+  tl6.play();
+});
+document.querySelector(".day6Link").addEventListener("mouseout", function () {
+  tl6.reverse();
+});
+
+tl7.to(".day7", { "--scale": 1.2, duration: 0.5, transformOrigin: "center", ease: "circ.inOut", });
+document.querySelector(".day7Link").addEventListener("mouseover", function () {
+  tl7.play();
+});
+document.querySelector(".day7Link").addEventListener("mouseout", function () {
+  tl7.reverse();
+});
+
+tl8.to(".day8", { "--scale": 1.2, duration: 0.5, transformOrigin: "center", ease: "circ.inOut", });
+document.querySelector(".day8Link").addEventListener("mouseover", function () {
+  tl8.play();
+});
+document.querySelector(".day8Link").addEventListener("mouseout", function () {
+  tl8.reverse();
+});
+
+tl9.to(".day9", { "--scale": 1.2, duration: 0.5, transformOrigin: "center", ease: "circ.inOut", });
+document.querySelector(".day9Link").addEventListener("mouseover", function () {
+  tl9.play();
+});
+document.querySelector(".day9Link").addEventListener("mouseout", function () {
+  tl9.reverse();
+});
+
+tl10.to(".day10", { "--scale": 1.2, duration: 0.5, transformOrigin: "center", ease: "circ.inOut", });
+document.querySelector(".day10Link").addEventListener("mouseover", function () {
+  tl10.play();
+});
+document.querySelector(".day10Link").addEventListener("mouseout", function () {
+  tl10.reverse();
 });
