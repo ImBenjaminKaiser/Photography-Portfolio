787f8bfe01d20e41eadaf28a0edb4d9a447badd0
Benjamin Kaiser <bnkaiser04@gmail.com>
2023-05-20 00:01:58 -0400

    add hover animation for day1

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ function updatemenu() {
       document.getElementById('menu').style.borderRadius = '0px';
     }
   }
+
+
+var tl = gsap.timeline({paused: true});
+
+tl.to(".day1", {"--scale": 1.2, duration: 1, transformOrigin: "center"});
+
+document.querySelector(".day1Link").addEventListener("mouseover", function() {
+ // play the timeline
+ tl.play();
+});
+
+document.querySelector(".day1Link").addEventListener("mouseout", function() {
+ // reverse the timeline
+ tl.reverse();
+});
